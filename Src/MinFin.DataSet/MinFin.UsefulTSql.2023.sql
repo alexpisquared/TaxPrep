685008f092eeeae7e19813da8d71bcce027fc35d
alexpisqueared <alex.pigida@outlook.com>
2023-11-15 17:59:11 -0500

    Create MinFin.UsefulTSql.2023.sql

---
+++ Src/MinFin.DataSet/MinFin.UsefulTSql.2023.sql
@@ -0,0 +1,19 @@
+﻿USE MinFinInv
+go
+
+SELECT     AcntHist.InvAccountID, COUNT(*) AS Cnt, MAX(AcntHist.Balance) AS Max, MIN(AcntHist.Balance) AS Min, InvAccount.SrcAccount, InvAccount.TrgAccount, InvAccount.OpenedOn, InvAccount.ClosedOn, InvAccount.BalanceOpening, InvAccount.BalanceClosing, InvAccount.Institution, 
+                  InvAccount.AcntType, InvAccount.AcntHolder, InvAccount.Advisor, InvAccount.Notes
+FROM        AcntHist INNER JOIN
+                  InvAccount ON AcntHist.InvAccountID = InvAccount.ID
+WHERE     (AcntHist.Balance <> 0)
+GROUP BY AcntHist.InvAccountID, InvAccount.SrcAccount, InvAccount.TrgAccount, InvAccount.OpenedOn, InvAccount.ClosedOn, InvAccount.BalanceOpening, InvAccount.BalanceClosing, InvAccount.Institution, InvAccount.AcntType, InvAccount.AcntHolder, InvAccount.Advisor, InvAccount.Notes
+ORDER BY Max DESC
+
+select sum(max) as TtlMax, sum(min) as TtlMin from (
+SELECT     AcntHist.InvAccountID, COUNT(*) AS Cnt, MAX(AcntHist.Balance) AS Max, MIN(AcntHist.Balance) AS Min, InvAccount.SrcAccount, InvAccount.TrgAccount, InvAccount.OpenedOn, InvAccount.ClosedOn, InvAccount.BalanceOpening, InvAccount.BalanceClosing, InvAccount.Institution, 
+                  InvAccount.AcntType, InvAccount.AcntHolder, InvAccount.Advisor, InvAccount.Notes
+FROM        AcntHist INNER JOIN
+                  InvAccount ON AcntHist.InvAccountID = InvAccount.ID
+WHERE     (AcntHist.Balance <> 0)
+GROUP BY AcntHist.InvAccountID, InvAccount.SrcAccount, InvAccount.TrgAccount, InvAccount.OpenedOn, InvAccount.ClosedOn, InvAccount.BalanceOpening, InvAccount.BalanceClosing, InvAccount.Institution, InvAccount.AcntType, InvAccount.AcntHolder, InvAccount.Advisor, InvAccount.Notes
+) as kljl
