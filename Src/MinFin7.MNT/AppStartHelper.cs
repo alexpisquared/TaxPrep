842113c53178054f34dcbcafcf7e7325368c3fa9
alexpisqueared <alex.pigida@outlook.com>
2024-01-07 12:07:15 -0500

    ++

--- Src/MinFin7.MNT/AppStartHelper.cs
+++ Src/MinFin7.MNT/AppStartHelper.cs
@@ -6,7 +6,7 @@ public static class AppStartHelper
   {
     _ = services.AddSingleton<IConfigurationRoot>(ConfigHelper.AutoInitConfigFromFile());
 
-    _ = services.AddSingleton<ILogger>(sp => SeriLogHelper.InitLoggerFactory((sp.GetRequiredService<IConfigurationRoot>()[CfgName.LogFolder] ?? "Logs").Replace("..", $"{Assembly.GetExecutingAssembly().GetName().Name![..6]}.{VersionHelper.Env()}.{Environment.UserName[..3]}.."), "+Verbose -Info -Warning +Error -ErNT -11mb -Infi").CreateLogger<MainNavView>());
+    _ = services.AddSingleton<ILogger>(sp => SeriLogHelper.CreateLogger<MainNavView>(/*Settings.Default.LogLevels*/));
 
     _ = services.AddSingleton<IBpr, Bpr>(); // _ = VersionHelper_.IsDbgAndRBD ? services.AddSingleton<IBpr, Bpr>() : services.AddSingleton<IBpr, BprSilentMock>();
 
