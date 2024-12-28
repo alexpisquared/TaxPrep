337b535f4afe9ea66497f457a0c346c9954c72d6
alexpisqueared <alex.pigida@outlook.com>
2024-12-28 17:00:57 -0500

    AAV window style

--- Src/MinFin7MdiLib/Views/TxCategoryAssignerVw.xaml.cs
+++ Src/MinFin7MdiLib/Views/TxCategoryAssignerVw.xaml.cs
@@ -307,7 +307,15 @@ If you want to DEBUG or Run with the current Package available, just set your pa
   }
 
   static async Task<bool> IsStillTyping(TextBox textbox) { var prev = textbox.Text; await Task.Delay(750); return prev != textbox.Text; }
-  void UpdateTitle(TimeSpan took, [CallerMemberName] string? cmn = "") => WriteLine(Title = $"Ctgry {_loadedCatgry,-12} {_core.Count,9:N0} rows    sum {_core.Where(r => r.TxCategoryIdTxt == "UnKn").Sum(r => r.TxAmount),9:N0} / {_core.Sum(r => r.TxAmount),-9:N0} \t selects {_selectTtl,9:N2} \t {took.TotalSeconds,4:N2}s \t {cmn} \t\t {VersionHelper.CurVerStr}");
+  void UpdateTitle(TimeSpan took, [CallerMemberName] string? cmn = "")
+  {
+    WriteLine(Title = $"Ctgry {_loadedCatgry,-12} {_core.Count,9:N0} rows    sum {_core.Where(r => r.TxCategoryIdTxt == "UnKn").Sum(r => r.TxAmount),9:N0} / {_core.Sum(r => r.TxAmount),-9:N0} \t selects {_selectTtl,9:N2} \t {took.TotalSeconds,4:N2}s \t {cmn} \t\t {VersionHelper.CurVerStr}");
+    h0.Text = $"Ctgry {_loadedCatgry,-12} ";
+    h1.Text = $"{_core.Count,9:N0} rows    ";
+    h2.Text = $"sum {_core.Where(r => r.TxCategoryIdTxt == "UnKn").Sum(r => r.TxAmount),9:N0} / {_core.Sum(r => r.TxAmount),-9:N0} \t ";
+    h3.Text = $"selects {_selectTtl,9:N2} \t {took.TotalSeconds,4:N2}s \t ";
+    h4.Text = $"{VersionHelper.CurVerStr}";
+  }
 
   async void OnReLoad(object s, RoutedEventArgs e)
   {
@@ -864,7 +872,7 @@ public static class DbSaveMsgBox
   public static int CheckAskSave(DbContext db, int maxRowsToShow = 32, MessageBoxButton btn = MessageBoxButton.YesNoCancel)
   {
     var rowsSaved = (int)MsgBoxDbRslt.Unknown;
-    retry:
+  retry:
     try
     {
       //_bpr.Ok();
@@ -894,7 +902,7 @@ public static class DbSaveMsgBox
   }
   public static int TrySaveAsk(DbContext db, string note)
   {
-    retry:
+  retry:
     try
     {
       var sw = Stopwatch.StartNew();
