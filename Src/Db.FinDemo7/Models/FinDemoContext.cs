531fccc34f5b145e502e0edce71462dbabad9a99
alexpisqueared <alex.pigida@outlook.com>
2024-12-28 21:54:39 -0500

    Update FinDemoContext.cs

--- Src/Db.FinDemo7/Models/FinDemoContext.cs
+++ Src/Db.FinDemo7/Models/FinDemoContext.cs
@@ -109,10 +109,6 @@ public partial class FinDemoContext : DbContext
 
     public virtual DbSet<ZTest> ZTests { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Server=.\\SqlExpress;Database=FinDemo;Trusted_Connection=True;TrustServerCertificate=Yes;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<BalAmtHist>(entity =>
